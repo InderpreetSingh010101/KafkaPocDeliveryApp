462f3f7b20d70dc322db71f2b6820bcfc74db089
InderpreetS <inder17375@gmail.com>
2025-03-16 15:01:59 +0530

    ConsumerService:Location consumer added for kafka

---
+++ consumerDeliveryLocation/src/main/java/com/producerDeliveryLocation/producerDeliveryLocation/ConsumerDeliveryLocationApplication.java
@@ -0,0 +1,13 @@
+package com.producerDeliveryLocation.producerDeliveryLocation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsumerDeliveryLocationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConsumerDeliveryLocationApplication.class, args);
+	}
+
+}
